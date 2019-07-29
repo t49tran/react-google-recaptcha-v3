5c7ce90f3ecdd596b9cd37a5edaf7819ff90a5ca
t49tran <tranthaiduong1210@gmail.com>
2019-07-29 17:34:22 +1000

    style: re-order example

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,12 @@
-import * as React from "react";
-import * as ReactDom from "react-dom";
-import { GoogleRecaptchaExample } from "./google-recaptcha-example";
-import { GoogleReCaptchaProvider } from "../src/google-recaptcha-provider";
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { GoogleReCaptchaProvider } from '../src/google-recaptcha-provider';
+import { GoogleRecaptchaExample } from './google-recaptcha-example';
 
 ReactDom.render(
   <GoogleReCaptchaProvider reCaptchaKey="6Ld2oX4UAAAAACqWA9-3ostCypVeQRd4JPIT6dZR">
     <h2>Google Recaptcha Example</h2>
     <GoogleRecaptchaExample />
   </GoogleReCaptchaProvider>,
-  document.getElementById("app")
+  document.getElementById('app')
 );
